87e2da382aeaa6f14d9fa7e3a4732837d7164bf2
ChaganYangoz <caganyangoz@gmail.com>
2023-08-24 16:37:23 +0300

    need to center popup

---
+++ script.js
@@ -0,0 +1,7 @@
+const addBook = document.querySelector("#add");
+
+addBook.onclick = () => {
+    document.getElementById("myForm").style.display = "flex";
+    document.getElementById("myForm").style.flexDirection = "row";
+    document.getElementById("myForm").style.justifyContent = "center";
+};
