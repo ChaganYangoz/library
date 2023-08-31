c38ec7147b03c7cfe4bdd9c122f6fafebb330290
ChaganYangoz <caganyangoz@gmail.com>
2023-08-31 15:42:35 +0300

    backdrop fixed

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 const addBook = document.querySelector("#add");
+const form = document.getElementById("myForm");
 
 addBook.onclick = () => {
-    document.getElementById("myForm").style.display = "flex";
-    document.getElementById("myForm").style.flexDirection = "row";
-    document.getElementById("myForm").style.justifyContent = "center";
+
+    form.style.display = 'flex';
+    form.style.flexDirection = "column";
+    form.style.gap = "5px";
+    form.style.alignItems = "center";
+    document.querySelector("#title").value='';
+    document.querySelector("#author").value='';
+    document.querySelector("#page").value='';
+};
+
+
+const addFormBtn = document.querySelector("#addForm");
+addFormBtn.onclick=()=>{
+    let title=document.querySelector("#title").value;
+    let author=document.querySelector("#author").value;
+    let page=document.querySelector("#page").value;
+
+    console.log(title+author+page);
+
+    form.style.display = 'none';
 };
